bd09a59c89ffcf028f081a04d2e7fedf7c74b4e1
Mehrdad Dindar <mehrdad.replay@gmail.com>
2025-06-26 02:17:11 +0330

    complate transaction base

---
+++ database/migrations/2025_06_25_222511_create_transactions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->enum('type',[0,1])->comment('1 = income , 0 = expense');
+            $table->integer('amount');
+            $table->string('category')->nullable();
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
