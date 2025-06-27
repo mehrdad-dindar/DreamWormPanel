f4c6f2030e6ea597ea5b3851b786491406994da5
Mehrdad Dindar <mehrdad.replay@gmail.com>
2025-06-27 19:42:40 +0330

    complate Transactions translation

--- app/Filament/Resources/TransactionResource.php
+++ app/Filament/Resources/TransactionResource.php
@@ -18,8 +18,13 @@ class TransactionResource extends Resource
 {
     protected static ?string $model = Transaction::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-banknotes';
+    protected static ?string $activeNavigationIcon = 'heroicon-s-banknotes';
+    protected static ?string $navigationLabel = 'تراکنش ها';
+    protected static ?string $breadcrumb = 'تراکنش ها';
+    protected static ?string $pluralModelLabel = 'تراکنش ها';
 
+    protected static ?string $modelLabel = 'تراکنش';
     public static function form(Form $form): Form
     {
         return $form
@@ -27,6 +32,7 @@ class TransactionResource extends Resource
                 Forms\Components\Hidden::make('user_id')
                     ->default(auth()->id()),
                 Forms\Components\TextInput::make('amount')
+                    ->translateLabel()
                     ->mask(RawJs::make('$money($input)'))
                     ->suffix('تومان')
                     ->mutateStateForValidationUsing(fn ($state) => str_replace(',', '', $state))
@@ -38,8 +44,10 @@ class TransactionResource extends Resource
                     ->default(0)
                     ->boolean(__('income'),__('expense')),
                 Forms\Components\TextInput::make('category')
+                    ->translateLabel()
                     ->maxLength(255),
                 Forms\Components\Textarea::make('description')
+                    ->translateLabel()
                     ->columnSpanFull(),
             ]);
     }
@@ -50,25 +58,31 @@ class TransactionResource extends Resource
             ->defaultSort('created_at', 'desc')
             ->columns([
                 Tables\Columns\TextColumn::make('user.name')
+                    ->translateLabel()
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('type')
+                    ->translateLabel()
                     ->formatStateUsing(fn($state) => match ((int)$state) {0 => __('expense'), 1 => __('income')})
                     ->color(fn($record) => $record->type ? 'success' : 'danger')
                     ->badge(),
                 Tables\Columns\TextColumn::make('amount')
+                    ->translateLabel()
                     ->numeric(locale: 'en')
                     ->suffix(' تومان')
                     ->sortable(),
                 Tables\Columns\TextColumn::make('category')
+                    ->translateLabel()
                     ->badge()
                     ->searchable(),
                 Tables\Columns\TextColumn::make('created_at')
-                    ->dateTime()
+                    ->translateLabel()
+                    ->jalaliDateTime('d F Y - H:i')
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\TextColumn::make('updated_at')
-                    ->dateTime()
+                    ->translateLabel()
+                    ->jalaliDateTime('d F Y - H:i')
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
             ])
