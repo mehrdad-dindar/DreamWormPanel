b7cc3436c739b5affcf3bb0d0fad365b4349c38d
Mehrdad Dindar <mehrdad.replay@gmail.com>
2025-07-14 23:44:40 +0330

    create batch chart

--- app/Filament/Resources/BatchResource.php
+++ app/Filament/Resources/BatchResource.php
@@ -130,7 +130,7 @@ class BatchResource extends Resource
     public static function table(Table $table): Table
     {
         return $table
-            ->defaultSort('created_at', 'desc')
+            ->defaultSort('egg_date', 'desc')
             ->columns([
                 Tables\Columns\TextColumn::make('batch_number')
                     ->translateLabel()
--- app/Filament/Resources/BatchResource/Pages/ListBatches.php
+++ app/Filament/Resources/BatchResource/Pages/ListBatches.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\BatchResource\Pages;
 
 use App\Filament\Resources\BatchResource;
+use App\Filament\Resources\BatchResource\Widgets\BatchCreatedChart;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
@@ -14,6 +15,13 @@ class ListBatches extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            BatchCreatedChart::class
         ];
     }
 }
