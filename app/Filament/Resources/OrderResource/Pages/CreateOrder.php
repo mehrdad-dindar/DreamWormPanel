734e42b730e5d833915c496ff188ab5ec79b7c8d
Mehrdad Dindar <mehrdad.replay@gmail.com>
2025-07-17 14:05:13 +0330

    complate order webpush

--- app/Filament/Resources/OrderResource/Pages/CreateOrder.php
+++ app/Filament/Resources/OrderResource/Pages/CreateOrder.php
@@ -5,9 +5,12 @@ namespace App\Filament\Resources\OrderResource\Pages;
 use App\Events\OrderCreated;
 use App\Filament\Resources\OrderResource;
 use App\Models\User;
+use App\Notifications\NewOrderPlaced;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Notification;
 use JetBrains\PhpStorm\NoReturn;
+use Spatie\Permission\Models\Role;
 
 class CreateOrder extends CreateRecord
 {
@@ -37,10 +40,16 @@ class CreateOrder extends CreateRecord
     protected function afterCreate(): void
     {
         event(new OrderCreated($this->record, $this->sms));
+        $this->sendNotificationToAdmins($this->record);
     }
 
 //    protected function getRedirectUrl(): string
 //    {
 //        return $this->getResource()::getUrl('index');
 //    }
+    private function sendNotificationToAdmins(?\Illuminate\Database\Eloquent\Model $record): void
+    {
+        $superAdmins = Role::findByName('super_admin')->users;
+        Notification::send($superAdmins, new NewOrderPlaced(order: $record));
+    }
 }
