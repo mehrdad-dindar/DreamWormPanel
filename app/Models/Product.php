64c330fd02fb0fbd4ea571d7d7a8688b5bfbd8c9
Mehrdad Dindar <mehrdad.replay@gmail.com>
2025-10-26 19:49:50 +0330

    add soft delete to products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
+    use SoftDeletes;
     protected $guarded;
 
     protected function helperText(): Attribute
